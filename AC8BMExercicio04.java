456a750f1cf136dabd65cc567c32a16d8229ee4d
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-05-04 22:28:29 -0300

    Create AC8BMExercicio04.java

---
+++ AC8BMExercicio04.java
@@ -0,0 +1,13 @@
+package application;
+
+//Class Equipamento
+public class AC8BMExercicio04 {
+	
+	private String nome;
+	private double preco;
+	
+	public AC8BMExercicio04 (String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
